1c47712249460e8a27ea460dc02bfad3caccde13
Richard Ore <eqliqandfriends@gmail.com>
2021-01-18 14:44:18 +0100

    added support for runtime check, get and set of properties

--- src/includes/native.h
+++ src/includes/native.h
@@ -10,10 +10,47 @@
 
 #define DEFINE_NATIVE(name) define_native(vm, #name, GET_NATIVE(name))
 
+#define NORMALIZE_IS_BOOL "bool"
+#define NORMALIZE_IS_NUMBER "number"
+#define NORMALIZE_IS_CHAR "char"
+#define NORMALIZE_IS_STRING "string"
+#define NORMALIZE_IS_CLOSURE "function"
+#define NORMALIZE_IS_INSTANCE "instance"
+#define NORMALIZE_IS_CLASS "class"
+#define NORMALIZE_IS_LIST "list"
+#define NORMALIZE_IS_DICT "dict"
+#define NORMALIZE_IS_OBJ "object"
+#define NORMALIZE_IS_FILE "file"
+
+#define NORMALIZE(token) NORMALIZE_##token
+
+#define ENFORCE_ARG_COUNT(name, d)                                             \
+  if (arg_count != d) {                                                        \
+    _runtime_error(vm, #name "() expects %d arguments, %d given", d,           \
+                   arg_count);                                                 \
+    return EMPTY_VAL;                                                          \
+  }
+
+#define ENFORCE_ARG_TYPE(name, i, type)                                        \
+  if (!type(args[i])) {                                                        \
+    _runtime_error(                                                            \
+        vm, #name "() expects argument %d as " NORMALIZE(type) ", %s given",   \
+        i + 1, value_type(args[i]));                                           \
+    return EMPTY_VAL;                                                          \
+  }
+
+#define RETURN_NIL return NIL_VAL
+#define RETURN return UNDEFINED_VAL
+#define RETURN_BOOL(v) return BOOL_VAL(v)
+#define RETURN_TRUE return BOOL_VAL(true)
+#define RETURN_FALSE return BOOL_VAL(false)
 #define RETURN_NUMBER(v) return NUMBER_VAL(v)
 
 DECLARE_NATIVE(time);
 DECLARE_NATIVE(microtime);
 DECLARE_NATIVE(id);
+DECLARE_NATIVE(hasprop);
+DECLARE_NATIVE(getprop);
+DECLARE_NATIVE(setprop);
 
 #endif
--- src/native.c
+++ src/native.c
@@ -3,6 +3,7 @@
 #include "compat/unistd.h"
 #include "native.h"
 #include "time.h"
+#include "vm.h"
 
 /**
  * time()
@@ -32,3 +33,53 @@ DECLARE_NATIVE(microtime) {
  * returns the unique identifier of value within the system
  */
 DECLARE_NATIVE(id) { RETURN_NUMBER((long)&args[0]); }
+
+/**
+ * hasprop(object: instance, name: string)
+ *
+ * returns true if object has the property name or not
+ */
+DECLARE_NATIVE(hasprop) {
+  ENFORCE_ARG_COUNT(hasprop, 2);
+  ENFORCE_ARG_TYPE(hasprop, 0, IS_INSTANCE);
+  ENFORCE_ARG_TYPE(hasprop, 1, IS_STRING);
+
+  b_obj_instance *instance = AS_INSTANCE(args[0]);
+  b_value dummy;
+  RETURN_BOOL(table_get(&instance->fields, args[1], &dummy));
+}
+
+/**
+ * getprop(object: instance, name: string)
+ *
+ * returns the property of object matching the name
+ * or nil if the object contains no property with a matching
+ * name
+ */
+DECLARE_NATIVE(getprop) {
+  ENFORCE_ARG_COUNT(getprop, 2);
+  ENFORCE_ARG_TYPE(getprop, 0, IS_INSTANCE);
+  ENFORCE_ARG_TYPE(getprop, 1, IS_STRING);
+
+  b_obj_instance *instance = AS_INSTANCE(args[0]);
+  b_value value;
+  table_get(&instance->fields, args[1], &value);
+  return value;
+}
+
+/**
+ * setprop(object: instance, name: string, value: any)
+ *
+ * sets the name property of object to value
+ * if the property already exist, it overwrites it
+ */
+DECLARE_NATIVE(setprop) {
+  ENFORCE_ARG_COUNT(setprop, 3);
+  ENFORCE_ARG_TYPE(setprop, 0, IS_INSTANCE);
+  ENFORCE_ARG_TYPE(setprop, 1, IS_STRING);
+
+  b_obj_instance *instance = AS_INSTANCE(args[0]);
+  b_value value;
+  table_set(vm, &instance->fields, args[1], args[2]);
+  RETURN;
+}
