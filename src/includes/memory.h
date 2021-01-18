915b9bf63f976a11d3fe7d1014a2899ea93f41ff
Richard Ore <eqliqandfriends@gmail.com>
2021-01-18 11:11:25 +0100

    done with gc for noew

--- src/includes/memory.h
+++ src/includes/memory.h
@@ -22,6 +22,6 @@ void *reallocate(b_vm *vm, void *pointer, size_t old_size, size_t new_size);
 void free_objects(b_vm *vm);
 void mark_object(b_vm *vm, b_obj *object);
 void mark_value(b_vm *vm, b_value value);
-void collect_garbage();
+void collect_garbage(b_vm *vm);
 
 #endif
