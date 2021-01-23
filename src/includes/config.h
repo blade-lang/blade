76f9c3c7f464de087989d69f0c1e0241e21b0ebf
Ore Richard Muyiwa <rore@appzonegroup.com>
2021-01-23 07:23:35 +0100

    updated tables load factor

--- src/includes/config.h
+++ src/includes/config.h
@@ -20,7 +20,11 @@
 #define MAX_FUNCTION_PARAMETERS 255
 #define FRAMES_MAX 256
 #define NUMBER_FORMAT "%.16g"
-#define TABLE_MAX_LOAD 0.75
+
+// Maximum load factor of 12/14
+// see: https://engineering.fb.com/2019/04/25/developer-tools/f14/
+#define TABLE_MAX_LOAD 0.85714286
+
 #define GC_HEAP_GROWTH_FACTOR 2
 
 #define USE_NAN_BOXING 1
