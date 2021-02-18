c858a683744422e2c4b12f58f58066b18a27f5f0
Richard Ore <eqliqandfriends@gmail.com>
2021-02-18 07:11:48 +0100

    fixed broken core module detection

--- src/core/pathinfo.h
+++ src/core/pathinfo.h
@@ -14,7 +14,7 @@
 char *get_exe_path();
 char *get_exe_dir();
 char *get_calling_dir();
-char *merge_paths(char *a, char *b, int length);
+char *merge_paths(char *a, char *b);
 bool file_exists(char *filepath);
 char *get_bird_filename(char *filename);
 char *get_filename(char *filepath);
