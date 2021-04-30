594000c3e9059b55300d13f259cff8b3eed3b7f8
Richard Ore <eqliqandfriends@gmail.com>
2021-04-30 13:20:42 +0100

    fixed for linux build

--- birdy.mk
+++ birdy.mk
@@ -105,6 +105,33 @@ else
 endif
 
 
+# Files.
+SUB_DIRS := core modules
+SRC_DIR := $(addprefix $(SOURCE_DIR)/, $(SUB_DIRS))
+BLD_DIRS := $(addprefix $(BUILD_DIR)/, $(SUB_DIRS))
+
+SOURCES = $(foreach sdir, $(SRC_DIR), $(wildcard $(sdir)/*.c))
+OBJECTS = $(patsubst $(SOURCE_DIR)/*.c, $(BUILD_DIR)/*.c, $(SOURCES))
+# INCLUDES = $(addprefix -I, $(addprefix $(SOURCE_DIR)/, $(SUB_DIRS)))
+
+# Adding the include paths
+# 
+# Curl needs to be available somehow on the device
+# and even though we try to supply the library for the build,
+# I have detected that those libraries may not work without
+# the -lcurl flag.
+CFLAGS += -I$(SOURCE_DIR)/core -I$(SOURCE_DIR) -Ideps/includes
+
+LIB_WIN32 :=
+
+# for mingw and cygwin
+ifeq ($(_OS),cygwin)
+	LIB_WIN32 = deps/lib/mingw/libpcre2-8.dll.a /usr/lib/libreadline.dll.a
+else ifeq ($(_OS),mingw)
+	CFLAGS += -lshlwapi
+	LIB_WIN32 = deps/lib/mingw/libpcre2-8.dll.a /mingw64/lib/libreadline.dll.a /mingw64/x86_64-w64-mingw32/lib/libshlwapi.a /mingw64/x86_64-w64-mingw32/lib/libws2_32.a /mingw64/x86_64-w64-mingw32/lib/libnetapi32.a
+endif
+
 LIB_STATICS:=
 
 ifeq ($(_OS),osx)
@@ -123,44 +150,19 @@ ifeq ($(_OS),linux)
 # for linux environments
 	SHARED_LIB_FLAGS := -Wl,-soname,libbirdy.so
 	SHARED_EXT := so	
-	CFLAGS += -Wno-return-local-addr -Wno-implicit-fallthrough -Wno-maybe-uninitialized -Wno-unused-result -Wno-sequence-point -lreadline
 # we want to make sure GCC compilers enable asprintf and vasprintf
 # We also need to link the math lib here
-	CFLAGS += -D_GNU_SOURCE -lm
+	CFLAGS +=  -D_GNU_SOURCE -Wno-return-local-addr -Wno-implicit-fallthrough -Wno-maybe-uninitialized -Wno-unused-result -Wno-sequence-point -lreadline
+	CFLAGS += -lm -lpcre2-8
 endif
 
-LIB_WIN32 :=
-
-# for mingw and cygwin
-ifeq ($(_OS),cygwin)
-	LIB_WIN32 = deps/lib/mingw/libpcre2-8.dll.a /usr/lib/libreadline.dll.a
-else ifeq ($(_OS),mingw)
-	CFLAGS += -lshlwapi
-	LIB_WIN32 = deps/lib/mingw/libpcre2-8.dll.a /mingw64/lib/libreadline.dll.a /mingw64/x86_64-w64-mingw32/lib/libshlwapi.a /mingw64/x86_64-w64-mingw32/lib/libws2_32.a /mingw64/x86_64-w64-mingw32/lib/libnetapi32.a
-endif
-
-# Files.
-SUB_DIRS := core modules
-SRC_DIR := $(addprefix $(SOURCE_DIR)/, $(SUB_DIRS))
-BLD_DIRS := $(addprefix $(BUILD_DIR)/, $(SUB_DIRS))
-
-SOURCES = $(foreach sdir, $(SRC_DIR), $(wildcard $(sdir)/*.c))
-OBJECTS = $(patsubst $(SOURCE_DIR)/*.c, $(BUILD_DIR)/*.c, $(SOURCES))
-# INCLUDES = $(addprefix -I, $(addprefix $(SOURCE_DIR)/, $(SUB_DIRS)))
-
-# Adding the include paths
-# 
-# Curl needs to be available somehow on the device
-# and even though we try to supply the library for the build,
-# I have detected that those libraries may not work without
-# the -lcurl flag.
-CFLAGS += -I$(SOURCE_DIR)/core -I$(SOURCE_DIR) -Ideps/includes -lcurl
+CFLAGS +=  -lcurl
 
 # Main build...
 build/$(NAME): $(OBJECTS) $(LIB_STATICS) $(LIB_WIN32)
 	@ printf "Building Bird in %s mode into %s for %s %s...\n" $(MODE) $(NAME) $(_OS) $(_ARCH)
 	@ printf "%s %s %s\n" $(CC) $@ "$(CFLAGS) $(LIB_STATICS) $(LIB_WIN32)"
 	@ mkdir -p build
-	@ $(CC) $(CFLAGS) $^ -o $@
+	@ $(CC) -v $^ -o $@ $(CFLAGS)
 
 .PHONY: default
