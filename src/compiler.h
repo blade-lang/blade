afe4ec28b67dbe38def8c32dc4dddf350e3291cb
Ore Richard Muyiwa <eqliqandfriends@gmail.com>
2021-07-26 13:36:04 +0100

    updated precedence comment

--- src/compiler.h
+++ src/compiler.h
@@ -7,7 +7,7 @@
 
 typedef enum {
   PREC_NONE,
-  PREC_ASSIGNMENT, // =, >=, <=, &=, |=, *=, +=, -=, /=, **=, %=, >>=, <<=, ^=,
+  PREC_ASSIGNMENT, // =, &=, |=, *=, +=, -=, /=, **=, %=, >>=, <<=, ^=, //=
   // ~=
   PREC_CONDITIONAL, // ?:
   PREC_OR,          // or
