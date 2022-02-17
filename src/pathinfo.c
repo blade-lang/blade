45b35c8711700f048bdb11bf156b830f79fcdb35
Richard Ore <eqliqandfriends@gmail.com>
2022-02-17 22:24:28 +0100

    now allowing .blade/libs and .blade/bin local directories to load modules and packages (former) and extensions (later) from the current working tree into the global scope.

--- src/pathinfo.c
+++ src/pathinfo.c
@@ -30,6 +30,7 @@
 #include <libgen.h>
 #include <limits.h>
 #include <mach-o/dyld.h>
+#include <printf.h>
 
 #endif
 
@@ -159,7 +160,8 @@ char *merge_paths(char *a, char *b) {
   // by checking b first, we guarantee that b is neither NULL nor
   // empty by the time we are checking a so that we can return a
   // duplicate of b
-  if(b == NULL || strlen(b) == 0) {
+  int len_b = (int)strlen(b);
+  if(b == NULL || len_b == 0) {
     free(final_path);
     return strdup(a); // just in case a is const char*
   } if(a == NULL || strlen(a) == 0) {
@@ -169,7 +171,7 @@ char *merge_paths(char *a, char *b) {
 
   final_path = append_strings(final_path, a);
 
-  if(b[0] != '.') {
+  if(!(len_b == 2 && b[0] == '.' && b[1] == 'b')) {
     final_path = append_strings(final_path, BLADE_PATH_SEPARATOR);
   }
   final_path = append_strings(final_path, b);
@@ -197,14 +199,48 @@ char *resolve_import_path(char *module_name, const char *current_file,
 
   // search system library if we are not looking for a relative module.
   if (!is_relative) {
-    // check in blade's default location
+
+    // firstly, search the local vendor directory for a matching module
+    char *root_dir = getcwd(NULL, 0);
+    // fixing last path / if exists (looking at windows)...
+    int root_dir_length = (int) strlen(root_dir);
+    if (root_dir[root_dir_length - 1] == '\\') {
+      root_dir[root_dir_length - 1] = '\0';
+    }
+
+    char *vendor_file = merge_paths(merge_paths(root_dir,
+          LOCAL_PACKAGES_DIRECTORY LOCAL_SRC_DIRECTORY), blade_file_name);
+    if (file_exists(vendor_file)) {
+      // stop a core library from importing itself
+      char *path1 = realpath(vendor_file, NULL);
+      char *path2 = realpath(current_file, NULL);
+
+      if (path1 != NULL) {
+        if (path2 == NULL || memcmp(path1, path2, (int) strlen(path2)) != 0)
+          return path1;
+      }
+    }
+
+    // or a matching package
+    char *vendor_index_file = merge_paths(merge_paths(merge_paths(root_dir,
+          LOCAL_PACKAGES_DIRECTORY LOCAL_SRC_DIRECTORY), module_name), blade_file_name);
+    if (file_exists(vendor_index_file)) {
+      // stop a core library from importing itself
+      char *path1 = realpath(vendor_index_file, NULL);
+      char *path2 = realpath(current_file, NULL);
+
+      if (path1 != NULL) {
+        if (path2 == NULL || memcmp(path1, path2, (int) strlen(path2)) != 0)
+          return path1;
+      }
+    }
+
+    // then, check in blade's default locations
     char *exe_dir = get_exe_dir();
-
-    // check the libs directory...
-
     char *blade_directory = merge_paths(exe_dir, LIBRARY_DIRECTORY);
-    char *library_file = merge_paths(blade_directory, blade_file_name);
 
+    // check blade libs directory for a matching module...
+    char *library_file = merge_paths(blade_directory, blade_file_name);
     if (file_exists(library_file)) {
       // stop a core library from importing itself
       char *path1 = realpath(library_file, NULL);
@@ -216,12 +252,10 @@ char *resolve_import_path(char *module_name, const char *current_file,
       }
     }
 
-    char *library_index_file =
-        merge_paths(merge_paths(blade_directory, module_name),
-                    get_blade_filename(LIBRARY_DIRECTORY_INDEX));
-
+    // check blade libs directory for a matching package...
+    char *library_index_file = merge_paths(merge_paths(blade_directory, module_name),
+           get_blade_filename(LIBRARY_DIRECTORY_INDEX));
     if (file_exists(library_index_file)) {
-      // stop a core library from importing itself
       char *path1 = realpath(library_index_file, NULL);
       char *path2 = realpath(current_file, NULL);
 
@@ -231,13 +265,10 @@ char *resolve_import_path(char *module_name, const char *current_file,
       }
     }
 
-    // check installed packages...
-
+    // check blade vendor directory installed module...
     char *blade_package_directory = merge_paths(exe_dir, PACKAGES_DIRECTORY);
     char *package_file = merge_paths(blade_package_directory, blade_file_name);
-
     if (file_exists(package_file)) {
-      // stop a core library from importing itself
       char *path1 = realpath(package_file, NULL);
       char *path2 = realpath(current_file, NULL);
 
@@ -247,12 +278,10 @@ char *resolve_import_path(char *module_name, const char *current_file,
       }
     }
 
-    char *package_index_file =
-        merge_paths(merge_paths(blade_package_directory, module_name),
-                    get_blade_filename(LIBRARY_DIRECTORY_INDEX));
-
+    // check blade vendor directory installed package...
+    char *package_index_file = merge_paths(merge_paths(blade_package_directory, module_name),
+          get_blade_filename(LIBRARY_DIRECTORY_INDEX));
     if (file_exists(package_index_file)) {
-      // stop a core library from importing itself
       char *path1 = realpath(package_index_file, NULL);
       char *path2 = realpath(current_file, NULL);
 
@@ -262,9 +291,9 @@ char *resolve_import_path(char *module_name, const char *current_file,
       }
     }
   } else {
-    // otherwise, search the relative path
-    char *relative_file = merge_paths(file_directory, blade_file_name);
 
+    // otherwise, search the relative path for a matching module
+    char *relative_file = merge_paths(file_directory, blade_file_name);
     if (file_exists(relative_file)) {
       // stop a user module from importing itself
       char *path1 = realpath(relative_file, NULL);
@@ -276,12 +305,10 @@ char *resolve_import_path(char *module_name, const char *current_file,
       }
     }
 
-    char *relative_index_file =
-        merge_paths(merge_paths(file_directory, module_name),
-                    get_blade_filename(LIBRARY_DIRECTORY_INDEX));
-
+    // or a matching package
+    char *relative_index_file = merge_paths(merge_paths(file_directory, module_name),
+          get_blade_filename(LIBRARY_DIRECTORY_INDEX));
     if (file_exists(relative_index_file)) {
-      // stop a user module from importing itself
       char *path1 = realpath(relative_index_file, NULL);
       char *path2 = realpath(current_file, NULL);
 
