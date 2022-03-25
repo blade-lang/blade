ff2fffdf1c9f3f5d96d25377410c7176810cc8e3
Richard Ore <eqliqandfriends@gmail.com>
2022-03-25 03:26:02 +0100

    used installed curl instead of bundled curl

--- packages/curl/curl.c
+++ packages/curl/curl.c
@@ -1,12 +1,19 @@
 #include <blade.h>
-#include "curl_setup.h"
 #include <curl/curl.h>
+
+void b__curl_module_preloader(b_vm *vm) {
+  curl_global_init(CURL_GLOBAL_ALL);
+}
+
+void b__curl_module_unloader(b_vm *vm) {
+  curl_global_cleanup();
+}
 
 DECLARE_MODULE_METHOD(curl__init) {
   RETURN;
 }
 
-CREATE_MODULE_LOADER(json) {
+CREATE_MODULE_LOADER(curl) {
   static b_func_reg module_functions[] = {
       {"init",   true,  GET_MODULE_METHOD(curl__init)},
       {NULL,    false, NULL},
@@ -17,8 +24,8 @@ CREATE_MODULE_LOADER(json) {
       .fields = NULL,
       .functions = module_functions,
       .classes = NULL,
-      .preloader = NULL,
-      .unloader = NULL
+      .preloader = &b__curl_module_preloader,
+      .unloader = &b__curl_module_unloader
   };
 
   return &module;
