b4175e35c448e60f6493b839ecaa3dc3bc672a97
Richard Ore <eqliqandfriends@gmail.com>
2023-04-21 04:15:10 +0100

    Update debug opcode names for better mnemonics.

--- src/debug.c
+++ src/debug.c
@@ -89,42 +89,42 @@ int disassemble_instruction(b_blob *blob, int offset) {
   uint8_t instruction = blob->code[offset];
   switch (instruction) {
     case OP_JUMP_IF_FALSE:
-      return jump_instruction("f_jump", 1, blob, offset);
+      return jump_instruction("fjump", 1, blob, offset);
     case OP_JUMP:
       return jump_instruction("jump", 1, blob, offset);
     case OP_TRY:
-      return try_instruction("i_try", blob, offset);
+      return try_instruction("itry", blob, offset);
     case OP_LOOP:
       return jump_instruction("loop", -1, blob, offset);
 
     case OP_DEFINE_GLOBAL:
-      return constant_instruction("d_glob", blob, offset);
+      return constant_instruction("dglob", blob, offset);
     case OP_GET_GLOBAL:
-      return constant_instruction("g_glob", blob, offset);
+      return constant_instruction("gglob", blob, offset);
     case OP_SET_GLOBAL:
-      return constant_instruction("s_glob", blob, offset);
+      return constant_instruction("sglob", blob, offset);
 
     case OP_GET_LOCAL:
-      return short_instruction("g_loc", blob, offset);
+      return short_instruction("gloc", blob, offset);
     case OP_SET_LOCAL:
-      return short_instruction("s_loc", blob, offset);
+      return short_instruction("sloc", blob, offset);
 
     case OP_GET_PROPERTY:
-      return constant_instruction("g_prop", blob, offset);
+      return constant_instruction("gprop", blob, offset);
     case OP_GET_SELF_PROPERTY:
-      return constant_instruction("g_props", blob, offset);
+      return constant_instruction("gprops", blob, offset);
     case OP_SET_PROPERTY:
-      return constant_instruction("s_prop", blob, offset);
+      return constant_instruction("sprop", blob, offset);
 
     case OP_GET_UP_VALUE:
-      return short_instruction("g_up_v", blob, offset);
+      return short_instruction("gupv", blob, offset);
     case OP_SET_UP_VALUE:
-      return short_instruction("s_up_v", blob, offset);
+      return short_instruction("supv", blob, offset);
 
     case OP_POP_TRY:
-      return simple_instruction("p_try", offset);
+      return simple_instruction("ptry", offset);
     case OP_PUBLISH_TRY:
-      return simple_instruction("pub_try", offset);
+      return simple_instruction("pubtry", offset);
 
     case OP_CONSTANT:
       return constant_instruction("load", blob, offset);
@@ -153,9 +153,9 @@ int disassemble_instruction(b_blob *blob, int offset) {
     case OP_DIVIDE:
       return simple_instruction("div", offset);
     case OP_F_DIVIDE:
-      return simple_instruction("f_div", offset);
+      return simple_instruction("fdiv", offset);
     case OP_REMINDER:
-      return simple_instruction("r_mod", offset);
+      return simple_instruction("rmod", offset);
     case OP_POW:
       return simple_instruction("pow", offset);
     case OP_NEGATE:
@@ -163,36 +163,36 @@ int disassemble_instruction(b_blob *blob, int offset) {
     case OP_NOT:
       return simple_instruction("not", offset);
     case OP_BIT_NOT:
-      return simple_instruction("b_not", offset);
+      return simple_instruction("bnot", offset);
     case OP_AND:
-      return simple_instruction("b_and", offset);
+      return simple_instruction("band", offset);
     case OP_OR:
-      return simple_instruction("b_or", offset);
+      return simple_instruction("bor", offset);
     case OP_XOR:
-      return simple_instruction("b_xor", offset);
+      return simple_instruction("bxor", offset);
     case OP_LSHIFT:
-      return simple_instruction("l_shift", offset);
+      return simple_instruction("lshift", offset);
     case OP_RSHIFT:
-      return simple_instruction("r_shift", offset);
+      return simple_instruction("rshift", offset);
     case OP_ONE:
       return simple_instruction("one", offset);
 
     case OP_CALL_IMPORT:
-      return short_instruction("c_import", blob, offset);
+      return short_instruction("cimport", blob, offset);
     case OP_NATIVE_MODULE:
-      return short_instruction("f_import", blob, offset);
+      return short_instruction("nimport", blob, offset);
     case OP_SELECT_IMPORT:
-      return short_instruction("s_import", blob, offset);
+      return short_instruction("simport", blob, offset);
       case OP_SELECT_NATIVE_IMPORT:
-      return short_instruction("sn_import", blob, offset);
+      return short_instruction("snimport", blob, offset);
     case OP_EJECT_IMPORT:
-      return short_instruction("e_import", blob, offset);
+      return short_instruction("eimport", blob, offset);
       case OP_EJECT_NATIVE_IMPORT:
-      return short_instruction("en_import", blob, offset);
+      return short_instruction("enimport", blob, offset);
     case OP_IMPORT_ALL:
-      return simple_instruction("a_import", offset);
+      return simple_instruction("aimport", offset);
       case OP_IMPORT_ALL_NATIVE:
-      return simple_instruction("an_import", offset);
+      return simple_instruction("animport", offset);
 
     case OP_ECHO:
       return simple_instruction("echo", offset);
@@ -205,13 +205,13 @@ int disassemble_instruction(b_blob *blob, int offset) {
     case OP_POP:
       return simple_instruction("pop", offset);
     case OP_CLOSE_UP_VALUE:
-      return simple_instruction("cl_up_v", offset);
+      return simple_instruction("clupv", offset);
     case OP_DUP:
       return simple_instruction("dup", offset);
     case OP_ASSERT:
       return simple_instruction("assrt", offset);
     case OP_POP_N:
-      return short_instruction("pop_n", blob, offset);
+      return short_instruction("popn", blob, offset);
 
       // non-user objects...
     case OP_SWITCH:
@@ -225,11 +225,11 @@ int disassemble_instruction(b_blob *blob, int offset) {
     case OP_DICT:
       return short_instruction("dict", blob, offset);
     case OP_GET_INDEX:
-      return byte_instruction("g_ind", blob, offset);
+      return byte_instruction("gind", blob, offset);
       case OP_GET_RANGED_INDEX:
-      return byte_instruction("gr_ind", blob, offset);
+      return byte_instruction("grind", blob, offset);
     case OP_SET_INDEX:
-      return simple_instruction("s_ind", offset);
+      return simple_instruction("sind", offset);
 
     case OP_CLOSURE: {
       offset++;
@@ -255,7 +255,7 @@ int disassemble_instruction(b_blob *blob, int offset) {
     case OP_INVOKE:
       return invoke_instruction("invk", blob, offset);
     case OP_INVOKE_SELF:
-      return invoke_instruction("invk_s", blob, offset);
+      return invoke_instruction("invks", blob, offset);
     case OP_RETURN:
       return simple_instruction("ret", offset);
 
@@ -264,15 +264,15 @@ int disassemble_instruction(b_blob *blob, int offset) {
     case OP_METHOD:
       return constant_instruction("meth", blob, offset);
     case OP_CLASS_PROPERTY:
-      return property_instruction("cl_prop", blob, offset);
+      return property_instruction("clprop", blob, offset);
     case OP_GET_SUPER:
-      return constant_instruction("g_sup", blob, offset);
+      return constant_instruction("gsup", blob, offset);
     case OP_INHERIT:
       return simple_instruction("inher", offset);
     case OP_SUPER_INVOKE:
-      return invoke_instruction("s_invk", blob, offset);
+      return invoke_instruction("sinvk", blob, offset);
     case OP_SUPER_INVOKE_SELF:
-      return byte_instruction("s_invk_s", blob, offset);
+      return byte_instruction("sinvks", blob, offset);
 
     default:
       printf("unknown opcode %d\n", instruction);
