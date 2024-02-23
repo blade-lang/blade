8a3e47d71cd240398cf889c45fb19b25ce7ea394
Richard Ore <eqliqandfriends@gmail.com>
2024-02-23 06:03:38 +0100

    Updated install script

--- src/blade.c
+++ src/blade.c
@@ -250,9 +250,10 @@ int main(int argc, char *argv[]) {
   int next_gc_start = DEFAULT_GC_START;
 
   if (argc > 1) {
-    int opt = opt = getopt(argc, argv, "hdeb:vg:wc:-");
-    printf("first opt = %d\n", opt);
-    while ((opt = getopt(argc, argv, "hdeb:vg:wc:-")) != -1 && opt != EOF) {
+    int opt;
+    optind = 1;
+
+    while ((opt = getopt(argc, argv, "hdeb:vg:wc:-")) != -1) {
       printf("Opt = %d, %d\n", opt, optind);
       switch (opt) {
         case 'h': {
